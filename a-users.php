00f59fcce3e896a87f5a1355797d3e645cc3d413
VictorMac <macoveschivictor@yahoo.com>
2016-06-11 14:56:37 +0300

    finished admin add page front-end & started admin users' page

--- a-users.php
+++ a-users.php
@@ -8,5 +8,25 @@ include 'includes/overall/header.php';
 ?>
 
 <h2>Admin Page - Users</h2>
+	<div class="inner">
+		<form method="post" action='a-users.php'>
+			<div class="side-form-left">
+				<ul id='search-form'>
+					<li>Search:<br>
+						<input type="text" name="search_box" style="width: 200px;">
+					</li>
+					<li>
+						<label><input type="radio" name="search">student</label>
+						<label><input type="radio" name="search">professor</label>
+					</li>
+					<input type="submit" value="Search">
+				</ul>
+			</div>
+		</form>
 
+	</div>
 <?php include 'includes/overall/footer.php' ?>
+
+
+
+
--- js/a-functions.js
+++ js/a-functions.js
@@ -1,107 +1,131 @@
-function addFunction(param, professors) {
-    switch (param){
-        case 1:
-            document.getElementById("load-form").innerHTML = addFormStudent();
-            break;
-        case 2:
-            document.getElementById("load-form").innerHTML = addFormProfessor();
-            break;
-        case 3:
-            document.getElementById("load-form").innerHTML = addFormCourse(professors);
-            break;
-    }
-}
+//function addFunction(param, values1, values2) {
+//	switch (param){
+//		case 1:
+//			document.getElementById("load-form").innerHTML = addFormStudent();
+//			break;
+//		case 2:
+//			document.getElementById("load-form").innerHTML = addFormProfessor();
+//			break;
+//		case 3:
+//			document.getElementById("load-form").innerHTML = addFormCourse(values1, values2);
+//			break;
+//	}
+//}
 
 function addFormStudent(){
-    var code='<form action="" method=post>'+
-            '<ul class="form-list" >'+
-                '<li>First Name:<br>'+
-                    '<input type="text" name="first_name" style="width: 200px;">'+
-                '</li>'+
-                '<li>Father Initial:<br>'+
-                    '<input type="text" name="parent_init" style="width: 200px;">'+
-                '</li>'+
-                '<li>Last Name:<br>'+
-                    '<input type="text" name="last_name" style="width: 200px;">'+
-                '</li>'+
-                '<li>Email:<br>'+
-                    '<input type="text" name="email" style="width: 200px;">'+
-                '</li>'+
-                '<li>Year:<br>'+
-                    '<input type="text" name="year" style="width: 200px;">'+
-                '</li>'+
-                '<li>Class:<br>'+
-                    '<input type="text" name="class" style="width: 200px;">'+
-                '</li>'+
-                '<li>'+
-                    '<input type="submit" value="Add">'+
-                '</li>'+
-            '</ul>'+
-        '</form>'
+	document.getElementById("load-form").innerHTML = '<form action="" method=post>'+
+			'<ul class="form-list" >'+
+				'<li>First Name:<br>'+
+					'<input type="text" name="first_name" style="width: 200px;">'+
+				'</li>'+
+				'<li>Father Initial:<br>'+
+					'<input type="text" name="parent_init" style="width: 200px;">'+
+				'</li>'+
+				'<li>Last Name:<br>'+
+					'<input type="text" name="last_name" style="width: 200px;">'+
+				'</li>'+
+				'<li>Email:<br>'+
+					'<input type="text" name="email" style="width: 200px;">'+
+				'</li>'+
+					'<div style="display: inline-block">' +
+						'Year:<br>'+
+						'<select name="year">'+
+							'<option value="1">1</option>'+
+							'<option value="2">2</option>'+
+							'<option value="3">3</option>'+
+						'</select>' +
+					'</div>' +
+					'<div style="display: inline-block; margin-left: 30px;">'+
+						'Half-Year:<br>'+
+						'<select name="semester">'+
+							'<option value="A">A</option>'+
+							'<option value="B">B</option>'+
+						'</select>' +
+					'</div>' +
+				'<li>' +
+						'<label><input type="radio" name="group">1</label>'+
+						'<label><input type="radio" name="group">2</label>'+
+						'<label><input type="radio" name="group">3</label>'+
+						'<label><input type="radio" name="group">4</label>'+
+						'<label><input type="radio" name="group">5</label>'+
+						'<label><input type="radio" name="group">6</label>'+
+						'<label><input type="radio" name="group">7</label>'+
+				'</li>'+
+				'<li>'+
+					'<input type="submit" value="Add">'+
+				'</li>'+
+			'</ul>'+
+		'</form>'
 
-    return code;
 }
 
 function addFormProfessor(){
-    var code='<form action="" method=post>'+
-        '<ul class="form-list" >'+
-        '<li>First Name:<br>'+
-        '<input type="text" name="first_name" style="width: 200px;">'+
-        '</li>'+
-        '<li>Father Initial:<br>'+
-        '<input type="text" name="parent_init" style="width: 200px;">'+
-        '</li>'+
-        '<li>Last Name:<br>'+
-        '<input type="text" name="last_name" style="width: 200px;">'+
-        '</li>'+
-        '<li>Email:<br>'+
-        '<input type="text" name="email" style="width: 200px;">'+
-        '</li>'+
-        '<input type="submit" value="Add">'+
-        '</li>'+
-        '</ul>'+
-        '</form>'
-
-    return code;
+	document.getElementById("load-form").innerHTML = '<form action="" method=post>'+
+		'<ul class="form-list" >'+
+			'<li>First Name:<br>'+
+				'<input type="text" name="first_name" style="width: 200px;">'+
+			'</li>'+
+			'<li>Father Initial:<br>'+
+				'<input type="text" name="parent_init" style="width: 200px;">'+
+			'</li>'+
+			'<li>Last Name:<br>'+
+				'<input type="text" name="last_name" style="width: 200px;">'+
+			'</li>'+
+			'<li>Email:<br>'+
+				'<input type="text" name="email" style="width: 200px;">'+
+			'</li>'+
+			'<li>'+
+				'<input type="submit" value="Add">'+
+			'</li>'+
+			'</ul>'+
+		'</form>'
 }
 
-function addFormCourse(professors){
-    var index;
-    var code='<form action="" method=post>'+
-        '<ul class="form-list" >'+
-            '<li>Course Name:<br>'+
-                '<input type="text" name="course_name" style="width: 200px;">'+
-            '</li>'+
-            '<li>' +
-                '<div style="display: inline-block">' +
-                    'Year:<br>'+
-                    '<select name="year">'+
-                        '<option value="1">1</option>'+
-                        '<option value="2">2</option>'+
-                        '<option value="3">3</option>'+
-                    '</select>' +
-                '</div>' +
-                '<div style="display: inline-block; margin-left: 30px;">'+
-                    'Semester:<br>'+
-                    '<select name="semester">'+
-                        '<option value="1">1</option>'+
-                        '<option value="2">2</option>'+
-                    '</select>' +
-                '</div>'+
-            '</li>'+
-            '<li>'+
-                'Head professors:<br>'+
-                '<select name="professors">';
-                    for (index = 0; index < professors.length; ++index) {
-                        code = code + '<option value="' + professors[index]['ID'] + '">' + professors[index]['first_name'] + ' ' + professors[index]['last_name'] + '</option>';
-                    }
-                code = code + '</select>'+
-            '</li>'+
-            '<li>' +
-                '<input type="submit" value="Add"></li>' +
-            '</ul>'+
-        '</form>'
-
-    return code;
-
+function addFormCourse(professors,courses){
+	var index;
+	var code = '<form action="" method=post>'+
+		'<ul class="form-list" >'+
+			'<li>Course Name:<br>'+
+				'<input type="text" name="course_name" style="width: 200px;">'+
+			'</li>'+
+			'<li>' +
+				'<div style="display: inline-block">' +
+					'Year:<br>'+
+					'<select name="year">'+
+						'<option value="1">1</option>'+
+						'<option value="2">2</option>'+
+						'<option value="3">3</option>'+
+					'</select>' +
+				'</div>' +
+				'<div style="display: inline-block; margin-left: 30px;">'+
+					'Semester:<br>'+
+					'<select name="semester">'+
+						'<option value="1">1</option>'+
+						'<option value="2">2</option>'+
+					'</select>' +
+				'</div>'+
+			'</li>'+
+			'<li>'+
+				'Head professor:<br>'+
+				'<select name="professors"><option value=""></option>';
+					for (index = 0; index < professors.length; ++index) {
+						code = code + '<option value="' + professors[index]['ID'] + '">' + professors[index]['first_name'] + ' ' + professors[index]['last_name'] + '</option>';
+					}
+				code = code + '</select>'+
+			'</li>'+
+			'<li>'+
+				'Assistant Teacher for:<br>' +
+				'<div class="assist-form" style=\'overflow:auto; height:100px;\'>'+
+					'<form>'
+						for (index = 0; index < courses.length; ++index){
+							code = code + '<input type="checkbox"'+ courses[index]['course_id'] + '">' + courses[index]['course_name'] + '<br>';
+						}
+					code = code + '</form>' +
+				'</div>'+
+			'</li>' +
+			'<li>' +
+				'<input type="submit" value="Add"></li>' +
+			'</ul>'+
+		'</form>'
+	document.getElementById("load-form").innerHTML = code;
 }
