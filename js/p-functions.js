6da515fddff31e420fc77e168593df13eba9f4ff
Florin Mazilu <maziluflorin84@gmail.com>
2016-06-13 01:51:35 +0300

    Changes to professors profile page 2

--- js/p-functions.js
+++ js/p-functions.js
@@ -1,23 +1,10 @@
 function addEditForm(course){
 	document.getElementById("load-form").innerHTML = '<form action="" method=post>'+
-<<<<<<< HEAD
-		'<ul id="form-list" >'+
-			'<li>Course Name:<br>' +
-				'<input type="text" name="course_name" style="width: 200px;" value="' + course['course_name'] + '">' +
-			'</li>' +
-			'<li>Year:<br>' +
-				'<input type="text" name="year" style="width: 200px;" value="' + course['year'] + '">' +
-			'</li>' +
-			'<li>Semester:<br>' +
-				'<input type="text" name="semester" style="width: 200px;" value="' + course['semester'] + '">' +
-			'</li>' +
-=======
 		'<ul id="form-list" >' +
 			'<input type="hidden" name="course_id" value="' + course['course_id'] + '">'+
 			'<li>Course Name:<br>' +
 				'<input type="text" name="course_name" style="width: 200px;" value="' + course['course_name'] + '">' +
 			'</li>' +
->>>>>>> 7c2153d4f26bfb8b633af07bd91fac84bb145b24
 			'<li>Number of course evaluations:<br>'+
 				'<input type="text" name="course_ev_no" style="width: 200px;" value="' + course['course_ev_no'] + '">' +
 			'</li>' +
--- p-profile.php
+++ p-profile.php
@@ -6,31 +6,9 @@ if (!logged_in()) {
 }
 include 'includes/overall/header.php';
 
-<<<<<<< HEAD
-$data = return_user_data($_SESSION['user_id'], $_SESSION['account_type']);
-$user_id = $_SESSION['user_id'];
-$result = $mysqli->query("SELECT * FROM `courses`");
-$rows = array();
-$ids = array();
-while ($row = $result->fetch_assoc()) {
-	$head_prof_id = $row['head_prof_id'];
-	if ($head_prof_id == $user_id) {
-		$row['head_prof_name'] = $data['first_name'].' '.$data['last_name'];
-		$rows[] = $row;
-	} else {
-		$head_profs = $mysqli->query("SELECT * FROM `professors` WHERE `ID` = '$head_prof_id'");
-		while ($head_prof = $head_profs->fetch_assoc())
-			$row['head_prof_name'] = $head_prof['first_name'].' '.$head_prof['last_name'];
-		$ids = explode(';', $row['assist_prof_ids']);
-		foreach ($ids as $value)
-			if ($value == $user_id)
-				$rows[] = $row;
-	}
-=======
 $messages = array();
 if(empty($_POST) === false) {
 	$messages = update_courses();
->>>>>>> 7c2153d4f26bfb8b633af07bd91fac84bb145b24
 }
 $data = return_user_data($_SESSION['user_id'], $_SESSION['account_type']);
 $user_id = $_SESSION['user_id'];
@@ -59,11 +37,7 @@ $rows = professor_courses($data, $user_id);
 		</table>
 
 		<h2>Courses</h2>
-<<<<<<< HEAD
-		<table class="table-data" cellspacing="0">
-=======
 		<table class="table-data courses" cellspacing="0">
->>>>>>> 7c2153d4f26bfb8b633af07bd91fac84bb145b24
 			<tr>
 				<th>Course Name</th>
 				<th>Head Professor</th>
@@ -71,27 +45,19 @@ $rows = professor_courses($data, $user_id);
 				<th>Edit Course</th>
 			</tr>
 			<?php
-<<<<<<< HEAD
-			foreach ($rows as $prof) {
-				echo
-				'<tr>'.
-					'<td>'.$prof['course_name'].'</td>'.
-					'<td>'.$prof['head_prof_name'].'</td>'.
-					'<td></td>';
-				if ($prof['head_prof_id'] == $user_id)
-					echo
-					'<td><input type="submit"  onclick=\'addEditForm('.json_encode($prof).')\' value="Edit"></td>';
-=======
+			$courses_classes = explode(';', $data['course_classes']);
 			foreach ($rows as $row) {
 				echo
 				'<tr>'.
 					'<td>'.$row['course_name'].'</td>'.
-					'<td>'.$row['head_prof_name'].'</td>'.
-					'<td></td>';
+					'<td>'.$row['head_prof_name'].'</td>';
+				foreach ($courses_classes as $course_classes) {
+//		to continue!!!!!!!			$co
+				}
+				echo '<td>'.$data['course_classes'].'</td>';
 				if ($row['head_prof_id'] == $user_id)
 					echo
 					'<td><input type="submit"  onclick=\'addEditForm('.json_encode($row).')\' value="Edit"></td>';
->>>>>>> 7c2153d4f26bfb8b633af07bd91fac84bb145b24
 				else
 					echo
 					'<td>N/A</td>';
@@ -105,14 +71,10 @@ $rows = professor_courses($data, $user_id);
 	<div class="side-form-right">
 		<div class="side-right-inner">
 			<div id="load-form">
-<<<<<<< HEAD
-
-=======
 				<?php
 				if(!empty($messages))
 					echo output_errors($messages);
 				?>
->>>>>>> 7c2153d4f26bfb8b633af07bd91fac84bb145b24
 			</div>
 		</div>
 	</div>
